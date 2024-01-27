f592efd7f134c7e7abefb67880da51d7b176be99
Fatemeh910 <fatemehkhoshnod91@gmail.com>
2024-01-27 13:20:20 +0330

    Ejra shodane event click roye dokme register

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 const loginButton = document.querySelector('login-btn');
 const registerButton = document.querySelector('register-btn');
+const formTitle = document.querySelector('form-title');
+//EVENT
+
+registerButton.addEventListener('click',() =>{
+ formTitle.innerHTML = "registerform";
+})
