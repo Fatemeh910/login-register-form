c1737e1b65800b5be55862683464d403074f7afa
Fatemeh910 <fatemehkhoshnod91@gmail.com>
2024-01-31 19:00:49 +0330

    Matn eror baraye login alert

--- main.js
+++ main.js
@@ -5,70 +5,75 @@ const loginForm = document.querySelector('.login-form');
 const registerForm = document.querySelector('.register-form');
 const eyeButtons = document.querySelectorAll('.eye-btn');
 const navigateLinks = document.querySelectorAll('.navigate-link')
+
 //LOGIN ELEMENT
-const loginEmailInput = document.querySelector('.login-email-input');
-const loginEmailAlert = document.querySelector('.login-email-alert');
+const loginEmailInput = document.getElementById('login-email-input');
+const loginEmailAlert = document.getElementById('login-email-alert');
 
 //SHOW REGISTER FORM WHEN CLICK TO REGISTER BUTTON
 registerButton.addEventListener("click", () => {
     formTitle.innerText = "Register Form";
 
-//TOGLE FORM BUTTONS
+    //TOGLE FORM BUTTONS
     registerButton.classList.add('active');
     loginButton.classList.remove("active");
 
-//SHOW REGISTER FORM AND HIDE LOGIN FORM
+    //SHOW REGISTER FORM AND HIDE LOGIN FORM
     registerForm.classList.add("active");
     loginForm.classList.remove("active");
 });
 
 //SHOW LOGIN FORM WHEN CLICK TO LOGIN BUTTON
-loginButton.addEventListener('click',() =>{
+loginButton.addEventListener('click', () => {
     formTitle.innerText = "Login Form";
 
-//TOGLE FORM BUTTONS
+    //TOGLE FORM BUTTONS
     loginButton.classList.add("active");
     registerButton.classList.remove('active');
 
-//SHOW REGISTER FORM AND HIDE REGISTER FORM
+    //SHOW REGISTER FORM AND HIDE REGISTER FORM
     loginForm.classList.add('active');
     registerForm.classList.remove('active');
 });
 
 //SHOW PASSWORD WHEN CLICK EYE BUTTONs
-eyeButtons.forEach(eye=>{
-  eye.addEventListener('click', () => {
-    const input = eye.previousElementSibling;
-    if(input.type === 'password'){
-        //CHANGE INPUT TYPE INTO TEXT
-        input.type = 'text';
-        eye.firstElementChild.classList.replace('fa-eye','fa-eye-slash');
-    }else{
-        //CHANGE INPUT TYPE INTO PASSWORD
-        input.type = 'password'
-        eye.firstElementChild.classList.replace('fa-eye-slash','fa-eye');
-    }
-  });
+eyeButtons.forEach(eye => {
+    eye.addEventListener('click', () => {
+        const input = eye.previousElementSibling;
+        if (input.type === 'password') {
+            //CHANGE INPUT TYPE INTO TEXT
+            input.type = 'text';
+            eye.firstElementChild.classList.replace('fa-eye', 'fa-eye-slash');
+        } else {
+            //CHANGE INPUT TYPE INTO PASSWORD
+            input.type = 'password'
+            eye.firstElementChild.classList.replace('fa-eye-slash', 'fa-eye');
+        }
+    });
 });
 //TOGLE between login and register forms
-navigateLinks.forEach(link =>{
-    link.addEventListener('click',(e)=>{
+navigateLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
         e.preventDefault();
         const dataLink = link.dataset.link;
 
-        if(dataLink === "register"){
-           registerButton.click();
-        }else if(dataLink === "login"){
+        if (dataLink === "register") {
+            registerButton.click();
+        } else if (dataLink === "login") {
             loginButton.click();
         }
     });
 });
 
 //validate login email
-loginEmailInput.addEventListener("input", () =>{
-const emailFormat =
- /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
- if(loginEmailInput.value.match(emailFormat)){
-    loginEmailInput.classList.add('wrong-input');
- }
-});
+loginEmailInput.addEventListener('input', () =>{
+const emailFormat = 
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+    if(!loginEmailInput.value.match(emailFormat)){
+        loginEmailInput.classList.add('wrong-input');
+        loginEmailAlert.innerText = "Enter valid email baby";
+    }else{
+
+    }
+})
